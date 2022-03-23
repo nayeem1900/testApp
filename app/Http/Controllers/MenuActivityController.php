cd116794a2557b60d3a5812654fc4a6353f4ba13
majadshohagh <majadul1989@gmail.com>
2022-03-23 09:06:06 +0600

    Role Management System

---
+++ app/Http/Controllers/MenuActivityController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MenuActivity;
+use Illuminate\Http\Request;
+
+class MenuActivityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MenuActivity  $menuActivity
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MenuActivity $menuActivity)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MenuActivity  $menuActivity
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MenuActivity $menuActivity)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MenuActivity  $menuActivity
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MenuActivity $menuActivity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MenuActivity  $menuActivity
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MenuActivity $menuActivity)
+    {
+        //
+    }
+}
